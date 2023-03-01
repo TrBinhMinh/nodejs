bc546fe666c9d09194bba6a089ec0dd8b2db5eda
nguyen duc manh <nguyen.duc@ascendcorp.com>
2023-03-01 17:50:57 +0700

    update to lesson 13

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ const url = require("url");
 
 ////////////////////////////////////////////
 // SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -38,9 +42,8 @@ const server = http.createServer((req, res) => {
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
   } else if (pathName === "/api") {
-    fs.readFile(`${__dirname}/dev-data/data.json`);
-
-    res.end("API");
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
