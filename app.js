726498c2a01f9a4c933ffc3d7877d0404cd959e2
nguyen duc manh <nguyen.duc@ascendcorp.com>
2023-03-14 18:18:24 +0700

    update to lesson 54

--- app.js
+++ app.js
@@ -29,6 +29,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((tour) => tour.id === id);
+
+  if (!tour) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
