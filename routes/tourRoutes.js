6898c35c210209103ee382135e4f1a406c1293bf
TrBinhMinh <bminh9900@gmail.com>
2023-04-05 23:00:10 +0700

    update to lesson 105

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -19,7 +19,7 @@ router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/tour-stats').get(getTourStats);
 
-router.route('/monthly-plan').get(getMonthlyPlan);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/').get(getAllTours).post(createTour);
 
