34376477cb3363669318e750cc64162e343e8c24
nguyen.tuan@ascendcorp.com <nguyen.tuan@ascendcorp.com>
2023-05-26 18:06:14 +0700

    update to lesson 131

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,6 +10,7 @@ const {
   getMonthlyPlan,
   // checkId,
 } = require('../controllers/tourController');
+const { protect } = require('../controllers/authController');
 
 const router = express.Router();
 
@@ -21,7 +22,7 @@ router.route('/tour-stats').get(getTourStats);
 
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(protect, getAllTours).post(createTour);
 
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
