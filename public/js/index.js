1fc0b20376f67407879a6d53a19625e9bb1f0208
nguyen.tuan@ascendcorp.com <nguyen.tuan@ascendcorp.com>
2023-07-04 18:51:42 +0700

    update to lesson 196

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,13 @@
 import '@babel/polyfill';
 import { login, logout } from './login';
 import { displayMap } from './leaflet';
+import { updateData } from './udpateSettings';
 
 // DOM ELEMENTS
 const map = document.getElementById('map');
-const loginForm = document.querySelector('.form');
+const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const updateUserForm = document.querySelector('.form-user-data');
 
 // VALUES
 
@@ -23,3 +25,13 @@ loginForm?.addEventListener('submit', (e) => {
 });
 
 logOutBtn?.addEventListener('click', logout);
+
+updateUserForm?.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(updateUserForm);
+  const name = formData.get('name');
+  const email = formData.get('email');
+
+  updateData(name, email);
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,13 +1,22 @@
 const express = require('express');
-const viewsController = require('../controllers/viewsController');
+const {
+  getAccount,
+  getOverview,
+  getTour,
+  getLoginForm,
+  updateUserData,
+} = require('../controllers/viewsController');
 const { isLoggedIn, protect } = require('../controllers/authController');
 
 const router = express.Router();
 
+router.get('/me', protect, getAccount);
+router.patch('/submit-user-data', protect, updateUserData);
+
 router.use(isLoggedIn);
 
-router.get('/', viewsController.getOverview);
-router.get('/tour/:slug', protect, viewsController.getTour);
-router.get('/login', viewsController.getLoginForm);
+router.get('/', getOverview);
+router.get('/tour/:slug', getTour);
+router.get('/login', getLoginForm);
 
 module.exports = router;
