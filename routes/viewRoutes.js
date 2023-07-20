a06c81e2b095d7d4d7a45fb2a9ced67aa361bf26
mai.tha <mai.tha@ascendcorp.com>
2023-07-20 17:13:36 +0700

    update to lesson 216

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,17 +5,20 @@ const {
   getTour,
   getLoginForm,
   updateUserData,
+  getMyTours,
 } = require('../controllers/viewsController');
 const { isLoggedIn, protect } = require('../controllers/authController');
+const { createBookingCheckout } = require('../controllers/bookingController');
 
 const router = express.Router();
 
 router.get('/me', protect, getAccount);
+router.get('/my-tours', protect, getMyTours);
 router.patch('/submit-user-data', protect, updateUserData);
 
 router.use(isLoggedIn);
 
-router.get('/', getOverview);
+router.get('/', createBookingCheckout, getOverview);
 router.get('/tour/:slug', getTour);
 router.get('/login', getLoginForm);
 
